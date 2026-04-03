e6a74768ef9a0a70fbb34fe1a3523c7e32a7f9d1
Andrei <danaila95andrei@gmail.com>
2026-04-04 00:04:43 +0300

    Almost finished

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 const mainDiv = document.querySelector(".mainDiv");
+const btn = document.querySelector("button");
 
-for (let i = 1; i <= 16 * 16; i++){
-    const childDiv = document.createElement("div");
-    childDiv.classList = "childDiv";
-    childDiv.addEventListener("mouseover", () => {childDiv.setAttribute("style", "background: gray;")});
-    mainDiv.appendChild(childDiv);
+function createGrid(l) {
+    for (let i = 1; i <= l * l; i++){
+        const childDiv = document.createElement("div");
+        childDiv.classList = "childDiv";
+        childDiv.style.height = `${90/l}vh`;
+        childDiv.style.width = `${90/l}vh`;
+        childDiv.addEventListener("mouseover", () => {childDiv.style.backgroundColor = "gray";});
+        mainDiv.appendChild(childDiv);
+    }
 }
+
+createGrid(16);
+
+btn.addEventListener("click", () =>{
+    const nr = Number(window.prompt("Choose the number of squares per side", ""));
+
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.firstChild);
+    }
+
+    createGrid(nr);
+});
